5ac52adc595af3ec7ba6b34947bc333b0337f152
Mauricio-Veloso <mauriciojr_veloso@hotmail.com>
2022-06-07 16:29:25 -0300

    Primeiro commit: Soma e Subtração adicionados

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,11 @@
-public class Calculadora{
+public class Calculadora {
+
+	public double soma(double numeroA, double numeroB) {
+		return numeroA + numeroB;
+	}
+	
+	public double subtracao(double numeroA, double numeroB) {
+		return numeroA - numeroB;
+	}
 
 }
