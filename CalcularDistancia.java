7533eeae8cca48f261b5a278440fe3fd8e70a773
Mauricio-Veloso <mauriciojr_veloso@hotmail.com>
2022-06-10 16:20:04 -0300

    Classes adicionadas por Mauricio Veloso

--- CalcularDistancia.java
+++ CalcularDistancia.java
@@ -1,29 +1,11 @@
-import java.util.Scanner; 
-import util.Calculadora;
-
 public class CalcularDistancia {
-    public static void main(String [] args){
+    private double distanciaAB = 80;
+    private double distanciaBC = 90;
+    
+    public double somandoDistancia(){
         Calculadora calculadora = new Calculadora();
 
-        java.util.Scanner scanner = new Scanner(System.in);
+        double soma = Calculadora.soma (quantiaAB, quantiaBC);
 
-        double distanciaAB;
-        
-        double distanciaBC; 
-
-        System.out.println("Por favor digite a distancia entre os pontos A e B em metros.");
-        distanciaAB = scanner.nextDouble();
-
-        System.out.println("Por favor digite a distancia entre os pontos B e C em metros");
-        distanciaBC = scanner.nextDouble();
-
-        scanner.nextLine();
-
-        double soma = Calculadora.soma (distanciaBC, distanciaAB);  
-
-        System.out.println("A distancia entre os pontos A e C é de "+ soma +" metros");
-        
-        scanner.close(); 
-        
     }
 }
